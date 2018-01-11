b474b7e0c65a5b5c03a042ac63a906a518eefd78
CavinHuang <943866180@qq.com>
2018-01-11 15:42:13 +0800

    env

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 // 导入webpack配置
 // 获取环境命令，并去除首尾空格
-const env = process.env.NODE_ENV.replace( /(\s*$)|(^\s*)/ig, "" );
+process.env.NODE_ENV = process.env.NODE_ENV ? process.env.NODE_ENV : 'prod';
+const env = process.env.NODE_ENV.replace(/(\s*$)|(^\s*)/ig, "");
 // 根据环境变量引用相关的配置文件
-module.exports = require( `./config/webpack.config.${env}.js` )
+module.exports = require(`./config/webpack.config.${env}.js`)
